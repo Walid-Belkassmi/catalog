862c5235c7396170cea4620122ae53693d837d97
Walid-Belkassmi <belkassmi.walid@gmail.com>
2022-08-02 17:38:22 +0200

    files

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+import movies from "../movies.json"
+
+const Home = () => {
+  
+  return (
+    <>
+      <h1>Movies list</h1>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/pages/Movie.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+const Movie = () => {
+  return (
+    <>
+      <h1>Movie</h1>
+    </>
+  );
+};
+
+export default Movie;
