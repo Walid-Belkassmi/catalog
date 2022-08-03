3247a1b7d8a5ec93063b3d07b522a52b035252b3
Walid-Belkassmi <belkassmi.walid@gmail.com>
2022-08-03 12:10:44 +0200

    css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,16 +4,16 @@ import catalog from "../catalog.json";
 
 const Home = () => {
   return (
-    <div className="container d-flex flex-column align-items-center justify-content-center my-5 text-center">
+    <div className="home-page container d-flex flex-column align-items-center justify-content-center my-5 text-center">
       <header className="mb-5">
-        <h1 className="text-warning">Movies</h1>
+        <h1 className="home-h1 text-warning">Movies</h1>
       </header>
-      <main className="m-5">
+      <main className="m-5 home">
         <ul>
           {catalog.map((movie) => {
             console.log(movie);
             return (
-              <li className=" home-li cursor" key={movie.title}>
+              <li className="cursor" key={movie.title}>
                 <Link
                   className="decoration text-light"
                   to={`/movie/${movie.id}`}
